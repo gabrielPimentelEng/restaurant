449b44d31139a1533cdf2335e133fe244c513d62
gaku <gabrielpimenteleng@hotmail.com>
2024-04-30 12:48:06 -0300

    Created django rest framework and developed apis for creating and seeing categories, MenuItems and booking

--- restaurant_project/restaurant_app/models.py
+++ restaurant_project/restaurant_app/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User#,Group
 # Create your models here.
 
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
 
 class Menu(models.Model):
     name  = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6,decimal_places=2)
     inventory = models.SmallIntegerField()
     description = models.CharField(max_length=1000,default="")
+    category = models.ForeignKey(Category, on_delete=models.PROTECT,default=1)
 
     def __str__(self):
         return self.name
@@ -22,3 +28,9 @@ class Booking(models.Model):
 
     def __str__(self):
         return self.first_name
+    
+# class Rating(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     menuitem_id = models.SmallIntegerField()
+#     rating = models.SmallIntegerField()
+
