0ed3b1c10cfc557cccb5ba70acea75576736f918
gaku <gabrielpimenteleng@hotmail.com>
2024-05-02 18:23:40 -0300

    New endpoints for Groups management to add users to Manager group and Delivery group and delete thoses too with class based views

---
+++ restaurant_project/restaurant_DRF/services.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import Group
+
+def manage_user_group(user, group_name, add=True):
+    group = Group.objects.get(name=group_name)
+    if add:
+        user.groups.add(group)
+    else:
+        user.groups.remove(group)
+    user.save()
