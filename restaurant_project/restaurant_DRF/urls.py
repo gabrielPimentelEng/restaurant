8142d766acd9f8399052e58974453ae723e8e812
gaku <gabrielpimenteleng@hotmail.com>
2024-05-09 15:47:35 -0300

    Corrected a bug in OrderItemManagementNew in which a delivery crew didn t receive a message he/she couldn t change the delivery crew assigned to the order and changed the class name OrderItemManagementNew to OrderItemManagementSpecific

--- restaurant_project/restaurant_DRF/urls.py
+++ restaurant_project/restaurant_DRF/urls.py
@@ -5,9 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 
 urlpatterns = [
-    # path('menu',views.menu_item, name='menu-drf'),
     path('menu-items',views.MenuView.as_view(), name='menu-item'),
-    # path('menu-items/<str:name>',views.MenuItemView.as_view(), name='menu-item'),
     path('menu-items/<int:pk>',views.MenuItemView.as_view(), name='menu-item'),
     path('category', views.CategoriesView.as_view()),
     path('book', views.BookingView.as_view()),
@@ -16,15 +14,13 @@ urlpatterns = [
     path('rating-create/<int:pk>', views.RatingViewList.as_view()),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('groups/manager/users',views.UserGroupManagementView.as_view()),
     path('groups/manager/users',views.Managers.as_view()),
     path('groups/manager/users/<int:pk>',views.ManagersDelete.as_view()),
     path('groups/delivery-crew/users',views.DeliveryCrew.as_view()),
     path('groups/delivery-crew/users/<int:pk>',views.DeliveryCrewDelete.as_view()),
     path('cart/menu-items', views.CartManagement.as_view()),
     path('orders', views.OrderItemManagement.as_view()),
-    # path('orders/<int:pk>', views.OrderItemManagementNew.as_view()),
-    path('orders/<int:pk>', views.OrderItemManagementNew.as_view()),
+    path('orders/<int:pk>', views.OrderItemManagementSpecific.as_view()),
     path('test', views.Test.as_view()),
     
     #testing yet
